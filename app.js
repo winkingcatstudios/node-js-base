feddff9c110f666b3f791c8c09053ac25ac1a0e7
winkingcat <dankercher@gmail.com>
2022-04-04 09:59:08 -0400

    serving html files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -8,7 +9,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
 
 app.listen(3000);
