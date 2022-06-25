5b10685c4e6d534df375046b5ff5237a2a971415
winkingcat <dankercher@gmail.com>
2022-06-25 17:22:51 -0400

    exposure notice

--- util/database.js
+++ util/database.js
@@ -1,6 +1,7 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+// Exposed on Github, but restricted in IP allow list
 const mongoConnect = (callback) => {
   MongoClient.connect(
     "mongodb+srv://winkingcat:tabletopdontstop@winkingcattabletop.hp3zr.mongodb.net/?retryWrites=true&w=majority"
