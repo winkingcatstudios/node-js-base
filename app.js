0fc1aee729db936bee491d4270d480a97fd8a9ab
winkingcat <dankercher@gmail.com>
2022-06-25 18:09:21 -0400

    started refactor from MySQL to MongoDB

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database");
+const mongoConnect = require("./util/database").mongoConnect;
 
 const app = express();
 
@@ -22,7 +22,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client) => {
-  console.log(client);
+mongoConnect(() => {
   app.listen(3000);
 });
--- util/database.js
+++ util/database.js
@@ -1,18 +1,29 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
-// Exposed on Github, but restricted in IP allow list
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
     "mongodb+srv://winkingcat:tabletopdontstop@winkingcattabletop.hp3zr.mongodb.net/?retryWrites=true&w=majority"
   )
     .then((client) => {
-      console.log("Connected");
-      callback(client);
+      console.log("Connected!");
+      _db = client.db();
+      callback();
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
