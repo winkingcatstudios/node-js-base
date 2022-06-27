c53164a2e97019cef2c448eea1244bc67e60bc46
winkingcat <dankercher@gmail.com>
2022-06-27 12:19:28 -0400

    TypeError: User.findById(...).this is not a function

--- models/user.js
+++ models/user.js
@@ -1,16 +1,41 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
-const sequelize = require('../util/database');
+const ObjectId = mongodb.ObjectId;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+
+  save() {
+    const db = getDb();
+    let dbOp;
+    // if (this._id) {
+    //   dbOp = db
+    //     .collection("users")
+    //     .updateOne({ _id: this._id }, { $set: this });
+    // } else {
+    dbOp = db.collection("users").insertOne(this);
+    // }
+    return dbOp
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then(user => console.log(user))
+      .catch((err) => console.log(err));
+  }
+}
 
 module.exports = User;
