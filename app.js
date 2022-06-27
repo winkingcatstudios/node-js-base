cc7f100342ff0182b7964293a78ab3a72a52d6d1
winkingcat <dankercher@gmail.com>
2022-06-27 15:04:12 -0400

    Delete products from cart and Order from cart to Mongo

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('62b9d27ce38402c40b8d2b12')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
