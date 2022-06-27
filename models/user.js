e81d29a4f8882359b8ad0765a5f26ce155ccb50f
winkingcat <dankercher@gmail.com>
2022-06-27 17:15:53 -0400

    mongoDB upgrade complete

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-const mongodb = require("mongodb");
-const getDb = require("../util/database").getDb;
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
 const ObjectId = mongodb.ObjectId;
 
@@ -7,17 +7,17 @@ class User {
   constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
-    this.cart = cart;
+    this.cart = cart; // {items: []}
     this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db.collection("users").insertOne(this);
+    return db.collection('users').insertOne(this);
   }
 
   addToCart(product) {
-    const cartProductIndex = this.cart.items.findIndex((cp) => {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
       return cp.productId.toString() === product._id.toString();
     });
     let newQuantity = 1;
@@ -29,16 +29,15 @@ class User {
     } else {
       updatedCartItems.push({
         productId: new ObjectId(product._id),
-        quantity: newQuantity,
+        quantity: newQuantity
       });
     }
-
     const updatedCart = {
-      items: updatedCartItems,
+      items: updatedCartItems
     };
     const db = getDb();
     return db
-      .collection("users")
+      .collection('users')
       .updateOne(
         { _id: new ObjectId(this._id) },
         { $set: { cart: updatedCart } }
@@ -47,32 +46,32 @@ class User {
 
   getCart() {
     const db = getDb();
-    const productIds = this.cart.items.map((i) => {
+    const productIds = this.cart.items.map(i => {
       return i.productId;
     });
     return db
-      .collection("products")
+      .collection('products')
       .find({ _id: { $in: productIds } })
       .toArray()
-      .then((products) => {
-        return products.map((p) => {
+      .then(products => {
+        return products.map(p => {
           return {
             ...p,
-            quantity: this.cart.items.find((i) => {
+            quantity: this.cart.items.find(i => {
               return i.productId.toString() === p._id.toString();
-            }).quantity,
+            }).quantity
           };
         });
       });
   }
 
   deleteItemFromCart(productId) {
-    const updatedCartItems = this.cart.items.filter((item) => {
+    const updatedCartItems = this.cart.items.filter(item => {
       return item.productId.toString() !== productId.toString();
     });
     const db = getDb();
     return db
-      .collection("users")
+      .collection('users')
       .updateOne(
         { _id: new ObjectId(this._id) },
         { $set: { cart: { items: updatedCartItems } } }
@@ -82,20 +81,20 @@ class User {
   addOrder() {
     const db = getDb();
     return this.getCart()
-      .then((products) => {
+      .then(products => {
         const order = {
           items: products,
           user: {
-            _id: new ObjectId(this.id),
-            name: this.name,
-          },
+            _id: new ObjectId(this._id),
+            name: this.name
+          }
         };
-        return db.collection("orders").insertOne(order);
+        return db.collection('orders').insertOne(order);
       })
-      .then(() => {
+      .then(result => {
         this.cart = { items: [] };
         return db
-          .collection("users")
+          .collection('users')
           .updateOne(
             { _id: new ObjectId(this._id) },
             { $set: { cart: { items: [] } } }
@@ -106,21 +105,21 @@ class User {
   getOrders() {
     const db = getDb();
     return db
-      .collection("order")
-      .find({ "user._id": new ObjectId(this._id) })
+      .collection('orders')
+      .find({ 'user._id': new ObjectId(this._id) })
       .toArray();
   }
 
   static findById(userId) {
     const db = getDb();
     return db
-      .collection("users")
+      .collection('users')
       .findOne({ _id: new ObjectId(userId) })
-      .then((user) => {
+      .then(user => {
         console.log(user);
         return user;
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err);
       });
   }
