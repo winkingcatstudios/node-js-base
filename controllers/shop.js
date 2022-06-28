9d99c1e2f28b3bd7ad6a9f28021743e9bf6344f8
winkingcat <dankercher@gmail.com>
2022-06-28 16:32:33 -0400

    typos

--- controllers/shop.js
+++ controllers/shop.js
@@ -46,7 +46,6 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
-    .execPopulate()
     .then(user => {
       const products = user.cart.items;
       res.render('shop/cart', {
@@ -83,7 +82,6 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
-    .execPopulate()
     .then(user => {
       const products = user.cart.items.map(i => {
         return { quantity: i.quantity, product: { ...i.productId._doc } };
