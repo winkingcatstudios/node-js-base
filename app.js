b18c54a9261fd734ec3be71792aa00087c363bc0
winkingcat <dankercher@gmail.com>
2022-06-27 20:46:58 -0400

    order refactored for mongoose

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const errorController = require("./controllers/error");
-const User = require("./models/user");
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById("62ba3e4f6a92d29ceeccd71f")
-    .then((user) => {
+  User.findById('62ba3e4f6a92d29ceeccd71f')
+    .then(user => {
       req.user = user;
       next();
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 mongoose
   .connect(
-    "mongodb+srv://winkingcat:tabletopdontstop@winkingcattabletop.hp3zr.mongodb.net/shop?retryWrites=true&w=majority"
+    'mongodb+srv://winkingcat:tabletopdontstop@winkingcattabletop.hp3zr.mongodb.net/shop?retryWrites=true&w=majority'
   )
-  .then((result) => {
+  .then(result => {
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: "Winkingcat",
-          email: "winkingcatstudio@gmail.com",
+          name: 'Max',
+          email: 'max@test.com',
           cart: {
-            items: [],
-          },
+            items: []
+          }
         });
         user.save();
       }
     });
     app.listen(3000);
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(err);
   });
